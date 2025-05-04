ad7d9d795b79eadfe111bed2b5d37be8eb56d0d7
Cat Core <arthur.koalas@free.fr>
2025-05-04 17:38:27 +0200

    Fix mapping resolver ignoring namespace

--- src/main/java/io/github/fabriccompatibilitylayers/crossremapper/helpers/BABRIC_NEW_FORMATMappingResolver.java
+++ src/main/java/io/github/fabriccompatibilitylayers/crossremapper/helpers/BABRIC_NEW_FORMATMappingResolver.java
@@ -2,24 +2,41 @@ package io.github.fabriccompatibilitylayers.crossremapper.helpers;
 
 import fr.catcore.wfvaio.FabricVariants;
 import io.github.fabriccompatibilitylayers.modremappingapi.api.v2.MappingUtils;
+import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.MappingResolver;
 
 public class BABRIC_NEW_FORMATMappingResolver extends BaseMappingResolver {
     private static final String CONTEXT_ID = "cross-intermediary-" + FabricVariants.BABRIC_NEW_FORMAT;
 
     public static String mapClassName(MappingResolver resolver, String namespace, String className) {
+        if (namespace.equals("official")) {
+            return FabricLoader.getInstance().getMappingResolver().mapClassName(namespace, className);
+        }
+
         return replaceSlashesWithDots(MappingUtils.mapClass(CONTEXT_ID, replaceDotsWithSlashes(className)));
     }
 
-    public static String unmapClassName(MappingResolver resolver, String targetNamespace, String className) {
+    public static String unmapClassName(MappingResolver resolver, String namespace, String className) {
+        if (namespace.equals("official")) {
+            return FabricLoader.getInstance().getMappingResolver().unmapClassName(namespace, className);
+        }
+
         return replaceSlashesWithDots(MappingUtils.unmapClass(CONTEXT_ID, replaceDotsWithSlashes(className)));
     }
 
     public static String mapFieldName(MappingResolver resolver, String namespace, String owner, String name, String descriptor) {
+        if (namespace.equals("official")) {
+            return FabricLoader.getInstance().getMappingResolver().mapFieldName(namespace, owner, name, descriptor);
+        }
+
         return MappingUtils.mapField(CONTEXT_ID, replaceDotsWithSlashes(owner), name, descriptor).getName();
     }
 
     public static String mapMethodName(MappingResolver resolver, String namespace, String owner, String name, String descriptor) {
+        if (namespace.equals("official")) {
+            return FabricLoader.getInstance().getMappingResolver().mapMethodName(namespace, owner, name, descriptor);
+        }
+
         return MappingUtils.mapMethod(CONTEXT_ID, replaceDotsWithSlashes(owner), name, descriptor).getName();
     }
 }
